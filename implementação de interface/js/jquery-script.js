2c82595d78fb801f1af513cd817643efb26ac379
eduardo-barroca <115836586+eduardo-barroca@users.noreply.github.com>
2023-01-25 17:19:35 -0300

    Add files via upload

---
+++ implementação de interface/js/jquery-script.js
@@ -0,0 +1,14 @@
+//efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+
+    $("botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").slidetoggle("slow");
+        $("#section-login").slidetoggle("slow")
+        $("#botao-cadastrar").slidetoggle("hide")
+   
+});
+
+
+});
